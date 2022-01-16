83a87f095271b032dc6f504acbe851b19ddd3b67
Christophe Manin <christophe.manin@rcf.fr>
2022-01-16 19:34:42 +0100

    #03004 add ffmpeg streaming

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -17,7 +17,7 @@ const settings = {
 const sketch = ({ context, width, height }) => {
   const agents = []
 
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 50; i++) {
     const x = random.range(0, width)
     const y = random.range(0, height)
 
